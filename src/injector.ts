e7b53d66518695a65fb027aefa8f257357fca4fb
Ollie Pugh <oliver.pugh@mac.com>
2021-11-30 22:18:38 +0000

    PR amendments

--- src/injector.ts
+++ src/injector.ts
@@ -4,8 +4,8 @@ import { DocumentClient } from "aws-sdk/clients/dynamodb"
 export default abstract class Injector {
     static async init() {
         try {
-            const module = await import(`./env/${process.env.NODE_ENV}`);
-            for (const [key, value] of Object.entries(module.default)) {
+            const envVariables = await import(`./env/${process.env.NODE_ENV}`);
+            for (const [key, value] of Object.entries(envVariables.default)) {
                 Container.set(key, value);
             }
         }
